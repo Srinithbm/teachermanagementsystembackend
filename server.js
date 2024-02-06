e7f7b7df4bc4560f17b80f1367a5e4e011dea009
Srinith B M <121087818+Srinithbm@users.noreply.github.com>
2024-02-06 21:16:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,23 @@ const port = 3000;
 connectToDataBase();
 
 app.use(express.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://teacher-management-portal-front-end.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+
+// CORS middleware
+app.use(cors({
+  origin: 'https://teacher-management-portal-front-end.vercel.app',
+  methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+  credentials: true
+}));
+
+// API routes
 app.use("/api", apiRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running at port : ${port}`);

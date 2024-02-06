236972158f0f255176ce67284505fd577aca0072
Srinith B M <121087818+Srinithbm@users.noreply.github.com>
2024-02-06 21:19:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ connectToDataBase();
 app.use(express.json());
 
 // CORS middleware
-app.use(cors({
-  origin: 'https://teacher-management-portal-front-end.vercel.app/api',
-  methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
-  credentials: true
-}));
+app.get('/test-cors', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://teacher-management-portal-front-end.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.send('CORS test successful');
+});
 
 // API routes
 app.use("/api", apiRoutes);
